9b72829145ee0004f5cf19a74062abd7b6bd2ae3
Mickaël Le Baillif <1567582+demikl@users.noreply.github.com>
2025-04-13 10:37:08 +0200

    github actions for releasing v0.1.0 failed (#23)
    * github actions for releasing v0.1.0 failed
    Fixes #22
    
    * ruff format

--- src/tarnfui/__init__.py
+++ src/tarnfui/__init__.py
@@ -1,6 +1,6 @@
 import argparse
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __description__ = (
     "Kubernetes cost and carbon energy saver that selectively shutdown workloads during non-working hours "
     "on staging clusters"
